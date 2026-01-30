8584f07fbe5069cd96ef9bbcabddfa7e173a7d59
Dev-Nonsense0909688 <blackplasma001@gmail.com>
2026-01-30 10:34:05 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  console.log("IP:",
+    req.headers["x-forwarded-for"]?.split(",")[0] ||
+    req.socket.remoteAddress
+  );
+  console.log("UA:", req.headers["user-agent"]);
+
+  res.sendFile(new URL("./index.html", import.meta.url).pathname);
+});
+
+app.post("/client-log", (req, res) => {
+  console.log("Client data:", req.body);
+  res.sendStatus(200);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
